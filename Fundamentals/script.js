ddcd718a7d7871428d54e3b77f24035192bf39a1
hasanAbdullah9825 <hasan.techtomorrow@gmail.com>
2021-01-15 17:08:05 +0600

    type of variable

--- Fundamentals/script.js
+++ Fundamentals/script.js
@@ -3,3 +3,7 @@ if(js==="amazing")
 alert('js is fun');
 console.log(30+38-5+10);
 console.log('hello world');
+myName='hasan';
+console.log(myName);
+console.log(typeof myName);
+console.log(typeof js);
