622cb9567359fbe4ebf4c437b9263c8676c61587
hasanAbdullah9825 <hasan.techtomorrow@gmail.com>
2021-01-18 23:23:54 +0600

    template literals

--- Fundamentals/script.js
+++ Fundamentals/script.js
@@ -1,9 +1,14 @@
-let js="amazing";
-if(js==="amazing")
-alert('js is fun');
-console.log(30+38-5+10);
-console.log('hello world');
-myName='hasan';
-console.log(myName);
-console.log(typeof myName);
-console.log(typeof js);
+// let js="amazing";
+// if(js==="amazing")
+// alert('js is fun');
+// console.log(30+38-5+10);
+// console.log('hello world');
+// myName='hasan';
+// console.log(myName);
+// console.log(typeof myName);
+// console.log(typeof js);
+
+const firstName=`Hasan`;
+const lastName=`chowdhury`;
+
+console.log(`My first name is ${firstName} and my last name is ${lastName}`);
