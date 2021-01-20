e20ba07acb4b452dccabdc652e784ed097a7974a
hasanAbdullah9825 <hasan.techtomorrow@gmail.com>
2021-01-20 08:16:46 +0600

    control statement

--- Fundamentals/script.js
+++ Fundamentals/script.js
@@ -18,6 +18,61 @@
 
 // console.log(2+`5`);
 
-let x=5;
-console.log(x+10);
-console.log(String(x)+10);
+// let x=5;
+// console.log(x+10);
+// console.log(String(x)+10);
+
+// const age=prompt("whats your age");
+// if(age=== 18){
+//     console.log(`you are adult because your are ${age} years old`)
+// }
+// else {
+//     console.log(`you are not adult because your are ${age} years old`)
+// }
+
+const d1=96;
+const d2=108;
+const d3=89;
+
+const k1=88;
+const k2=91;
+const k3=110;
+
+averageD= (d1+d2+d3)/3;
+averagek=(k1+k2+k3)/3;
+
+if(averageD>averagek && (averageD>=10 || averagek>=100) ){
+    console.log(`Dolphins team is the winner`);
+}
+else if(averagek===averagek)
+{
+    console.log(`Drawwwwwww`);
+
+}
+
+else
+{
+    console.log(`Koalas is the winner`);
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
