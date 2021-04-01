8a4b9ba07ddaae86b39df8e78cb7782a1c89ca56
hasanAbdullah9825 <hasan.techtomorrow@gmail.com>
2021-04-01 13:09:15 +0600

    Automatic Type Conversion

--- Fundamentals/script.js
+++ Fundamentals/script.js
@@ -86,6 +86,20 @@ console.log(Number(inputyear)+5);
 console.log(Number('hasan'));//not a number
 console.log(typeof a); // undefined
 
+//type coercion or Automatic Type Conversion
+
+
+//5 + null    // returns 5         because null is converted to 0
+//"5" + null  // returns "5null"   because null is converted to "null"
+//"5" + 2     // returns "52"      because 2 is converted to "2"
+//"5" - 2     // returns 3         because "5" is converted to 5
+//"5" * "2"   // returns 10        because "5" and "2" are converted to 5 and 2
+
+
+console.log("5"+2);
+console.log("5"-2);
+console.log("a"-"b");
+console.log("5"*"2"+5+"1");
 
 
 
